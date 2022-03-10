ea00d310d94b530b5d43138a4b7a0575322bf4d3
Anthony Heier Laptop <anthony.heier78@gmail.com>
2022-03-10 11:25:57 -0600

    10MAR22

--- lab4/src/app/app.component.ts
+++ lab4/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lab4';
+  display:string = "";
   departments:Array<String>=[
     "Accounting",
     "Business",
@@ -14,5 +16,20 @@ export class AppComponent {
     "Human Resource",
     "Information Technology",
     "Marketing"
-  ]
+  ];
+
+  onSubmit(form:any){
+    console.log(form)
+    this.display = "{";
+    this.display += "\n\t\"firstName\": " + form.firstName
+    this.display += ",\n\t\"lastName\": " + form.lastName
+    this.display += ",\n\t\"email\": " + form.email
+    this.display += ",\n\t\"passord\": " + form.pass
+    this.display += ",\n\t\"phoneNumber\": " + form.phone
+    this.display += ",\n\t\"department\": " + form.department
+    this.display += ",\n\t\"gender\": " + form.gender
+    this.display += ",\n\t\"isSubscribed\": " + form.isSubscribed
+    this.display += "\n}"
+    console.log(this.display);
+  }
 }
